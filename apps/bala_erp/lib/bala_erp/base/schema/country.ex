15aa01c9403f7aee810b32f50eaba5d90ced4c68
Laszlo Balassa <b.lacika23@gmail.com>
2021-12-05 21:07:04 +0100

    [MOD] added reference field

--- apps/bala_erp/lib/bala_erp/base/schema/country.ex
+++ apps/bala_erp/lib/bala_erp/base/schema/country.ex
@@ -3,12 +3,14 @@ defmodule BalaErp.Base.Schema.Country do
   import Ecto.Changeset
 
   @currency BalaErp.Base.Schema.Currency
+  @addresses BalaErp.Base.Schema.Address
 
   schema "countries" do
     field :code, :string
     field :name, :string
 
     belongs_to :currency, @currency
+    has_many :addresses, @addresses
 
     timestamps()
   end
